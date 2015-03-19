a2478f90cb165423304dfa364da8d5257316c8bc
Ricard Sole <ricard.solecasas@gmail.com>
2015-03-19 08:21:04 +0100

    wip

--- test/test.js
+++ test/test.js
@@ -7,18 +7,31 @@ var rm = require('rimraf')
 var cordova = require('../')
 
 describe('gulp-cordovacli', function() {
+  var CONFIG_FILE = '../fixtures.json'
 
-  beforeEach(function(done) {
+  it('should emit an error if no commands and no file are provided', function(done) {
     gulp.src('.')
-      .pipe(cordova(['create', 'test/test', 'test.test.test', 'Test'], { silent: true }))
-      .on('close', function() {
-        process.chdir('test/test')
+      .pipe(cordova())
+      .on('error', function(err) {
+        assert(err)
         done()
       })
   })
 
+  beforeEach(function(done) {
+    gulp.src('.')
+      .pipe(cordova(['create', 'test/test', 'test.test.test', 'Test'], { silent: false }))
+      .on('close', function() {
+        gulp.src('.')
+          .pipe(cordova(['platform', 'add', 'browser'], { cwd: __dirname + '/test' }))
+          .on('close', function() {
+            done()
+          })
+      })
+  })
+
   afterEach(function(done) {
-    process.chdir('../../')
+    // process.chdir('../../')
     rm('test/test', function() {
       done()
     })
@@ -29,7 +42,7 @@ describe('gulp-cordovacli', function() {
 
     it('should run the commands from the configuration file', function(done) {
       gulp.src(CONFIG_FILE)
-        .pipe(cordova(false, { silent: true }))
+        .pipe(cordova(false, { silent: false }))
         .on('close', function() {
           assert.equal(true, fs.existsSync(__dirname + '/test/platforms/browser'))
 
@@ -39,7 +52,7 @@ describe('gulp-cordovacli', function() {
 
     it('explicit commands should override configuration file', function(done) {
       gulp.src(CONFIG_FILE)
-        .pipe(cordova(['build'], { silent: true }))
+        .pipe(cordova(['build'], { silent: false }))
         .on('close', function() {
           assert.equal(false, fs.existsSync(__dirname + '/test/platforms/browser'))
 
@@ -51,7 +64,7 @@ describe('gulp-cordovacli', function() {
   describe('without a configuration file', function() {
     it('should run the given commands', function (done) {
       gulp.src('.')
-        .pipe(cordova(['platforms', 'add', 'browser'], { silent: true }))
+        .pipe(cordova(['platforms', 'add', 'browser'], { silent: false }))
         .on('close', function() {
           assert.equal(true, fs.existsSync(__dirname + '/test/platforms/browser'))
 
@@ -69,7 +82,7 @@ describe('gulp-cordovacli', function() {
             "platform",
             "add",
             "browser"
-          ]], { silent: true }))
+          ]], { silent: false }))
         .on('close', function() {
           assert.equal(true, fs.existsSync(__dirname + '/test/platforms/browser'))
           assert.equal(true, fs.existsSync(__dirname + '/test/plugins/org.apache.cordova.device'))
